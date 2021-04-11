2f2900f53ee55c48ecf4b1283f4e9083342a22f2
marcus.zhang <marcuszhang@augmentum.com.cn>
2021-04-11 14:45:05 +0800

    feat(init): init serverless spring boot

---
+++ src/main/java/cc/alpgo/eat/MyHandler.java
@@ -0,0 +1,11 @@
+package cc.alpgo.eat;
+
+import com.qcloud.scf.runtime.AbstractSpringHandler;
+
+public class MyHandler extends AbstractSpringHandler {
+    @Override
+    public void startApp() {
+        System.out.println("start app");
+        DemoApplication.main(new String[]{""});
+    }
+}
